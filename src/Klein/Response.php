8054104072ce8fedf24d7f93f57e37adafbdcbee
Peter Kinnaird <peter@peterkinnaird.eu>
2014-02-25 12:21:23 +0000

    Fix some formatting and import DOMDocument
    DOMDocument needs to be imported as it is not in the correct namespace.

--- src/Klein/Response.php
+++ src/Klein/Response.php
@@ -11,6 +11,8 @@
 
 namespace Klein;
 
+use DOMDocument;
+
 /**
  * Response 
  * 
@@ -164,10 +166,10 @@ class Response extends AbstractResponse
 
 
 
-        $json = $this->xml_encode($object);
+        $xml = $this->xml_encode($object);
 
         $this->header('Content-Type', 'application/xml');
-        $this->body($json);
+        $this->body($xml);
 
         $this->send();
 
@@ -180,42 +182,41 @@ class Response extends AbstractResponse
      * Expanded from darklaunch.com
      */
     private function xml_encode($mixed, $domElement=null, $DOMDocument=null) {
-    if (is_null($DOMDocument)) {
-
-        $DOMDocument =new DOMDocument;
-        $DOMDocument->formatOutput = true;
-
-        $this->xml_encode($mixed, $DOMDocument, $DOMDocument);
-        return $DOMDocument->saveXML();
-    }
-    else {
-        if (is_array($mixed)) {
-            foreach ($mixed as $index => $mixedElement) {
-                if (is_int($index)) {
-                    if ($index === 0) {
-                        $node = $domElement;
-                    } else {
-                        $node = $DOMDocument->createElement($domElement->tagName);
-                        $domElement->parentNode->appendChild($node);
+        if (is_null($DOMDocument)) {
+    
+            $DOMDocument =new DOMDocument;
+            $DOMDocument->formatOutput = true;
+    
+            $this->xml_encode($mixed, $DOMDocument, $DOMDocument);
+            return $DOMDocument->saveXML();
+        } else {
+            if (is_array($mixed)) {
+                foreach ($mixed as $index => $mixedElement) {
+                    if (is_int($index)) {
+                        if ($index === 0) {
+                            $node = $domElement;
+                        } else {
+                            $node = $DOMDocument->createElement($domElement->tagName);
+                            $domElement->parentNode->appendChild($node);
+                        }
                     }
-                }
-                else {
-                    $plural = $DOMDocument->createElement($index);
-                    $domElement->appendChild($plural);
-                    $node = $plural;
-                    if (!(rtrim($index, 's') === $index)) {
-                        $singular = $DOMDocument->createElement(rtrim($index, 's'));
-                        $plural->appendChild($singular);
-                        $node = $singular;
+                    else {
+                        $plural = $DOMDocument->createElement($index);
+                        $domElement->appendChild($plural);
+                        $node = $plural;
+                        if (!(rtrim($index, 's') === $index)) {
+                            $singular = $DOMDocument->createElement(rtrim($index, 's'));
+                            $plural->appendChild($singular);
+                            $node = $singular;
+                        }
                     }
+     
+                    $this->xml_encode($mixedElement, $node, $DOMDocument);
                 }
- 
-                $this->xml_encode($mixedElement, $node, $DOMDocument);
+            }
+            else {
+                $domElement->appendChild($DOMDocument->createTextNode($mixed));
             }
         }
-        else {
-            $domElement->appendChild($DOMDocument->createTextNode($mixed));
-        }
     }
-}
 }
